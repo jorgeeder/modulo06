385bd64793f8260278b17a5cfad62c4cb9eee35e
jorgeeder <jorgeeder.js@gmail.com>
2019-11-06 01:20:05 -0300

    Performing navigation

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,7 +1,7 @@
 import Reactotron from 'reactotron-react-native';
 
 if (__DEV__) {
-  const tron = Reactotron.configure({ host: '192.168.1.104' })
+  const tron = Reactotron.configure({ host: '10.0.0.114' })
     .useReactNative()
     .connect();
 
